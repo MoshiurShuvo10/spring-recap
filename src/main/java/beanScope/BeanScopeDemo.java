185b0d56822a0767160b5b89a8ce76447a71554f
MoshiurShuvo10 <shuvorm87@gmail.com>
2022-05-02 00:57:43 +0600

    bean scope: prototype

--- src/main/java/beanScope/BeanScopeDemo.java
+++ src/main/java/beanScope/BeanScopeDemo.java
@@ -6,10 +6,18 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class BeanScopeDemo {
     public static void main(String[] args) {
         ApplicationContext applicationContext = new AnnotationConfigApplicationContext("beanScope") ;
-        SingletonDemo obj1 = applicationContext.getBean("singletonDemo", SingletonDemo.class);
-        SingletonDemo obj2 = applicationContext.getBean("singletonDemo", SingletonDemo.class);
-        System.out.println("obj1: "+obj1+" --- obj2: "+obj2);
-        if(obj1 == obj2)
-            System.out.println("same object..");
+        PrototypeDemo prototypeDemo1 = applicationContext.getBean("prototypeDemo", PrototypeDemo.class);
+        PrototypeDemo prototypeDemo2 = applicationContext.getBean("prototypeDemo", PrototypeDemo.class);
+        if(prototypeDemo1 == prototypeDemo2)
+            System.out.println("same..");
+        else System.out.println("not same");
+
+
+        // ---------- singleton -------------
+//        SingletonDemo obj1 = applicationContext.getBean("singletonDemo", SingletonDemo.class);
+//        SingletonDemo obj2 = applicationContext.getBean("singletonDemo", SingletonDemo.class);
+//        System.out.println("obj1: "+obj1+" --- obj2: "+obj2);
+//        if(obj1 == obj2)
+//            System.out.println("same object..");
     }
 }
